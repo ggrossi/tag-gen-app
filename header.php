2789d401e733057978175744c5284c95f23fcfaf
Pedro Kfuri <pedrokfuri@outlook.com>
2017-10-31 16:47:08 -0200

    bootstrap cdn

--- header.php
+++ header.php
@@ -1,14 +1,37 @@
 <head>
-	<title>O Incrível Gerador de Meta Tags</title>
 
-	<!-- Bootstrap -->
+	<!-- Meta Tags -->
+	<meta charset="utf-8">
+	<title>Título do Gerador de Meta Tags</title>
+	<meta name="description" content="Descrição do Gerador de Meta Tags">
+	<meta name="robots" content="index, follow">
+	<meta name="author" content="Rock Content">
+
+	<!-- OpenGraph -->
+	<meta property="og:title" content="Titulo da ferramenta"/>
+	<meta property="og:description" content="descricao da ferramenta"/>
+	<meta property="og:type" content=""/>
+	<meta property="og:url" content="url da ferramenta"/>
+	<meta property="og:image" content="url da imagem de og"/>
+
+	<!-- Twitter Card -->
+	<meta name="twitter:card" content="product">
+	<meta name="twitter:site" content="@rockcontent">
+	<meta name="twitter:title" content="Titulo da ferramenta">
+	<meta name="twitter:description" content="descricao da ferramenta">
+	<meta name="twitter:image" content="url da imagem pro twitter">
+
+	<!-- Bootstrap CDN -->
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script>
+
+	<!-- Bootstrap Offline -->
+	<!--<link rel="stylesheet" href="css/bootstrap.css">-->
+	<!--<script src="js/bootstrap.js"></script>-->
 
 	<!-- Custom Bootstrap -->
 	<link rel="stylesheet" href="css/custom.css">
 	<script src="js/custom.js"></script>
 
-	
 
 </head>
