9207cd6017d3d664e9f19c51b0bd87b93af88dcd
Pedro Kfuri <pedrokfuri@outlook.com>
2017-11-08 18:56:52 -0200

    deleted unused data and started content

--- generator.php
+++ generator.php
@@ -1,11 +1,28 @@
-<div class="container content">
+<div class="container-fluid content">
 	<div class="row text-center">
 
 		<div class="col-6">
-			aaaaa
+			<form id="user-input" class="content-box">
+				<div class="form-group">
+					<input type="text" class="form-control" id="exampleFormControlInput1" placeholder="Título da Página">
+				</div>
+				<div class="form-group">
+					<input type="text" class="form-control" id="exampleFormControlInput1" placeholder="Título da Página">
+				</div>
+				<div class="form-group">
+					<input type="text" class="form-control" id="exampleFormControlInput1" placeholder="Título da Página">
+				</div>
+				<div class="form-group">
+					<input type="text" class="form-control" id="exampleFormControlInput1" placeholder="Título da Página">
+				</div>
+				<div class="form-group">
+					<input type="text" class="form-control" id="exampleFormControlInput1" placeholder="Título da Página">
+				</div>
+			</form>
 		</div>
+
 		<div class="col-6">
-			aaaaa
+			
 		</div>
 
 	</div>
