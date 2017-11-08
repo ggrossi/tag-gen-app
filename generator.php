6baad02e6405a3e06cace654e495417213a59e0c
Pedro Kfuri <pedrokfuri@outlook.com>
2017-11-08 19:13:18 -0200

    container fluid on content

--- generator.php
+++ generator.php
@@ -1,29 +1,25 @@
-<div class="container-fluid content">
-	<div class="row text-center">
+<div class="container-fluid"> 
+	<div class="col-6">
+		<form id="user-input" class="content-box">
+			<div class="form-group">
+				<input type="text" class="form-control" id="exampleFormControlInput1" placeholder="Título da Página">
+			</div>
+			<div class="form-group">
+				<input type="text" class="form-control" id="exampleFormControlInput1" placeholder="Título da Página">
+			</div>
+			<div class="form-group">
+				<input type="text" class="form-control" id="exampleFormControlInput1" placeholder="Título da Página">
+			</div>
+			<div class="form-group">
+				<input type="text" class="form-control" id="exampleFormControlInput1" placeholder="Título da Página">
+			</div>
+			<div class="form-group">
+				<input type="text" class="form-control" id="exampleFormControlInput1" placeholder="Título da Página">
+			</div>
+		</form>
+	</div>
 
-		<div class="col-6">
-			<form id="user-input" class="content-box">
-				<div class="form-group">
-					<input type="text" class="form-control" id="exampleFormControlInput1" placeholder="Título da Página">
-				</div>
-				<div class="form-group">
-					<input type="text" class="form-control" id="exampleFormControlInput1" placeholder="Título da Página">
-				</div>
-				<div class="form-group">
-					<input type="text" class="form-control" id="exampleFormControlInput1" placeholder="Título da Página">
-				</div>
-				<div class="form-group">
-					<input type="text" class="form-control" id="exampleFormControlInput1" placeholder="Título da Página">
-				</div>
-				<div class="form-group">
-					<input type="text" class="form-control" id="exampleFormControlInput1" placeholder="Título da Página">
-				</div>
-			</form>
-		</div>
-
-		<div class="col-6">
+	<div class="col-6">
 			
-		</div>
-
 	</div>
 </div>
